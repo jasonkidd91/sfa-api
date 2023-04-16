ff4161ce116856f82b0fdaba34036c17d49d74ea
Wei Chuan Beh <kidd.beh@gmail.com>
2023-04-16 18:02:12 +0800

    Fix Swagger Error

--- src/main/java/com/wcbeh/sfa/controller/StashAwayController.java
+++ src/main/java/com/wcbeh/sfa/controller/StashAwayController.java
@@ -14,23 +14,26 @@ import java.util.List;
 @RequestMapping
 public class StashAwayController {
 
-    public static String SAMPLE_JSON = "{\"depositList\":[{\"customerId\":1,\"customerPortfolioId\":1,\"depositPlan\":\"ONE_TIME\",\"depositAmount\":500},{\"customerId\":2,\"customerPortfolioId\":3,\"depositPlan\":\"MONTHLY\",\"depositAmount\":200}],\"floatingList\":[{\"customerId\":1,\"customerPortfolioId\":2,\"depositAmount\":100},{\"customerId\":2,\"customerPortfolioId\":3,\"depositAmount\":\"-100\"}]}";
+    public static String SAMPLE_DATA = "{\"depositList\":[{\"customerId\":1,\"customerPortfolioId\":1,\"depositPlan\":\"ONE_TIME\",\"depositAmount\":500},{\"customerId\":2,\"customerPortfolioId\":3,\"depositPlan\":\"MONTHLY\",\"depositAmount\":200}],\"floatingList\":[{\"customerId\":1,\"customerPortfolioId\":2,\"depositAmount\":100},{\"customerId\":2,\"customerPortfolioId\":3,\"depositAmount\":\"-100\"}]}";
 
     @Autowired
     private StashAwayService stashAwayService;
 
     @GetMapping
     public ResponseEntity<String> main() {
-        String message = "Serving is running..." +
-                "<br/><br/>" +
-                "Check out the Frontend at: https://sfa-ui-jasonkidd91.vercel.app/" +
-                "<br/><br/>" +
-                "Check out the Swagger at: https://sfa-api.behwei.repl.co/swagger-ui/index.html" +
-                "<br/>" +
-                "<br/>[POST]: /deposits" +
-                "<br/>[SAMPLE DATA]: " + SAMPLE_JSON +
-                "<br/>";
-        return ResponseEntity.ok(message);
+        String frontendUrl = "https://sfa-ui-jasonkidd91.vercel.app/";
+        String swaggerUrl = "https://sfa-api.behwei.repl.co/swagger-ui/index.html";
+        String apiEndpoint = "[POST] /deposits";
+        String html = "<html>\n" +
+                "<body>\n" +
+                "<h1>Serving is running...</h1>\n" +
+                "<p>Check out the Frontend at: <a href='" + frontendUrl + "'>" + frontendUrl + "</a></p>\n" +
+                "<p>Check out the Swagger at: <a href='" + swaggerUrl + "'>" + swaggerUrl + "</a></p>\n" +
+                "<p>API Endpoint: " + apiEndpoint + "</p>\n" +
+                "<p>Sample Data for API Endpoint: " + SAMPLE_DATA + "</p>\n" +
+                "</body>\n" +
+                "</html>";
+        return ResponseEntity.ok(html);
     }
 
     @GetMapping("/all-user-portfolios")
